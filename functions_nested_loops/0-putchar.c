c24b1744edeee0c7226a98e21a3d38f6bdf828c7
emanuel6877 <emanuelmd922@gmail.com>
2023-10-19 10:42:21 -0400

    task 1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Write a program that prints Holberton, followed by a new*
+ *
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
