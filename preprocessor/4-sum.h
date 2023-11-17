bdbbbe0f152ab7777a24932a397769b52c030b44
emanuel6877 <emanuelmd922@gmail.com>
2023-11-16 22:48:28 -0400

    task 4

--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,5 +1,13 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef SUM_H
+#define SUM_H
 
-#define SUM(x, y) (x + y)
+/*
+ * File: 4-sum.h
+ * 
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
 
+#define SUM(x, y) ((x) + (y))
+
+#endif
